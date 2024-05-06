8efadfd3ef07ee381c0994be2dd3ebe9a35afdf4
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-05-06 12:38:26 +0300

    lesson 4

---
+++ app.js
@@ -0,0 +1,188 @@
+// setTimeout
+// document.body.style.background = "red";
+// setTimeout(() => 
+//     document.body.style.background = "" , 3000
+// )
+
+// let box = document.getElementsByClassName("box");
+// console.log(box.offsetWidth);
+
+// try {
+//     console.log(a);
+//     let a = 2 + 5;
+// } catch (err) {
+//     console.log(err.massage);
+// }
+
+// //  let arr = delta => () {
+
+// // }
+
+//masalalarr... 
+// 1masala
+// function sum(x,y) {
+//     console.log(x + y);
+// }
+// sum(2,3)
+
+// 2masala
+// function toSekunds(minutes) {
+//     console.log(minutes * 60);
+// }
+// toSekunds(5)
+
+// 3masala
+// function nextInteger(int) {
+//     console.log(int + 1);
+// }
+// nextInteger(2)
+
+// 4masala
+// function uchburchakYuzi(asos, balandlik) {
+//     console.log((asos * balandlik) / 2);
+// }
+// uchburchakYuzi(7, 4)
+
+// 5masala  
+// function ageToDays(age) {
+//     // code...
+//     console.log(age * 365);
+// }
+// ageToDays(65)
+
+// 6masala
+// function daraja(x){
+//     console.log(x**3);
+// }
+// daraja(5)
+
+// 7masala
+// function firstElement(arr) {
+//     arr.forEach((el) => {
+//         el = Number
+//             console.log(el);
+
+//     });
+// }
+
+// firstElement([1,2,3])
+
+// 8masala
+// function power(x,y) {
+// console.log(x * y);
+// }
+// power(10, 3)
+
+// 9masala
+// function hourToSekunds(hour) {
+//      let minut = 60;
+//      let sekund = 60;
+//      console.log(hour * minut * sekund);    
+// }
+// hourToSekunds(2)
+
+// 10masala
+// function uchinchiTomon(tomon1, tomon2) {
+//     console.log((tomon1 + tomon2) - 1);
+// }
+// uchinchiTomon(8,10)
+
+// 11masala
+// function qoldiq(son1, son2) {
+//     console.log(son1 % son2);
+// }
+// qoldiq(3, 2)
+
+// 12masala
+// function turtburchakYuzi(boyi, eni) {
+//     console.log(boyi * eni);
+// }
+// turtburchakYuzi(2,9)
+
+// 13masala
+// function stringQoshish(a) {
+//     a = "somethink"
+//     let s = a
+//     let string =( "somethink" + " " + " is better than nothing")
+//     console.log(string);
+// }
+// stringQoshish()
+
+// 14 masala...
+// function kvadrat(a){
+//     let kvadrati = a * a;
+//     console.log(kvadrati);
+// }
+// kvadrat(5)
+
+// 15masala
+// function tenglik(a){
+//    if(a <= 0){
+//     console.log(true);
+//    }else{
+//     console.log(false);
+//    }
+// }
+// tenglik(-1)
+
+// 16masala
+// function burchakla(a){
+//     console.log((a - 2) * 180);
+// }
+// burchakla(3)
+  
+// 17masalla
+// function ochkolar(x,y){
+//     x = x * 2;
+//     y = y * 3;
+//     console.log(x + y);
+// }
+// ochkolar(1, 1)
+
+// 18masala
+// function qoshaloq(a){
+//     a = "Edabit";
+//     let lyuboySoz = "javaScript"
+//     console.log(lyuboySoz + a);
+// }
+// qoshaloq()
+
+// 19masala
+// function anqlik(x,y){
+//     if(x + y < 100){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// anqlik(10, 91)
+
+// 20masala
+// function array(x) {
+//      x = 2;
+//     let arry = [];
+//     console.log(arry.push(x));
+
+// }
+// array()
+
+// 21masala
+// function oyoqlar(tovuq, qoy, sigir) {
+//     let sonit = 2;
+//     let soniq = 3;
+//     let sonis = 5;
+//     tovuq = 2 * sonit;
+//     qoy = 4 * soniq;
+//     sigir = 4 * sonis;
+//     let jami = tovuq + sigir + qoy;
+//     console.log(jami);
+// }
+// oyoqlar(2,3,5)
+
+// 22masala
+// function anqlik(){
+//     let anq = true && false;
+//     console.log(anq);
+// }
+// anqlik()
+
---
+++ note.tm
@@ -0,0 +1,5 @@
+window.navigator
+dom
+document
+setTimeout = ozggartradi vaqt bersa 
+
